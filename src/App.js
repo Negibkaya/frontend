2bfd2c09735ddd02f925d93b2a7175071b16c12a
Ivan <grigona4@mail.ru>
2023-05-01 13:23:28 +0300

    работает только для task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import axios from "axios";
+import { baseUrl } from "./utils/constant";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [updateUI, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/get`).then((res) => {
+      console.log(res.data);
+      setTasks(res.data);
+    });
+  }, [updateUI]);
+
+  const addTask = () => {
+    axios.post(`${baseUrl}/save`, { task: input }).then((res) => {
+      console.log(res.data);
+      setInput("");
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateMode = (id, text) => {
+    console.log(text);
+    setInput(text);
+    setUpdateId(id);
+  };
+
+  const updateTask = () => {
+    axios.put(`${baseUrl}/update/${updateId}`, { task: input }).then((res) => {
+      console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+      setUpdateId(null);
+      setInput("");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="title">Crud Operation</h1>
+
+      <div className="input_holder">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button type="submit" onClick={updateId ? updateTask : addTask}>
+          {updateId ? "Update Task" : "Add task"}
+        </button>
+
+        <ul>
+          {tasks.map((task) => (
+            <List
+              key={task._id}
+              id={task._id}
+              task={task.task}
+              setUpdateUI={setUpdateUI}
+              updateMode={updateMode}
+            />
+          ))}
+        </ul>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
